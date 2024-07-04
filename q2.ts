92658058d69568bc2cc810747bb014d9b373c605
PAO <natthachai_bubpa@cmu.ac.th>
2024-07-04 15:48:29 +0700

    1111

--- q2.ts
+++ q2.ts
@@ -1,18 +1,21 @@
-// define interface for Student object
-/* Your code here */
+  interface student{
+    name: string;
+    score: number;
+  }
+  
+  // assign interface/type to the function definition properly
+  function findTopNames(students) {
+    const filterscore = students.filter((st) => st.score > 8).map((st) =>st.name);
+    return filterscore;
+  }
 
-// assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
-}
+  // assign interface/type to the student1 object properly
+  const students1 = [
+    { name: "john", score: 10 },
+    { name: "jane", score: 9 },
+    { name: "jim", score: 8 },
+  ];
 
-// assign interface/type to the student1 object properly
-const students1 = [
-  { name: "john", score: 10 },
-  { name: "jane", score: 9 },
-  { name: "jim", score: 8 },
-];
+  console.log(findTopNames(students1));
 
-console.log(findTopNames(students1));
-
-module.exports = findTopNames;
+  module.exports = findTopNames;
